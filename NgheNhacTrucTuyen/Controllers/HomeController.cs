9ac54864a55df9825991f2ea17d475327d1b4768
Present111 <161811020+Present111@users.noreply.github.com>
2024-10-22 23:11:08 +0700

    add homepage

--- NgheNhacTrucTuyen/Controllers/HomeController.cs
+++ NgheNhacTrucTuyen/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NgheNhacTrucTuyen.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,20 +11,11 @@ namespace NgheNhacTrucTuyen.Controllers
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
+            DBcontextDataContext context = new DBcontextDataContext();
+            var theloai = context.TheLoais.ToList();
+            ViewBag.test = theloai;
+            var nhac = context.Nhacs.ToList();
+            ViewBag.jointable = nhac;
             return View();
         }
     }
