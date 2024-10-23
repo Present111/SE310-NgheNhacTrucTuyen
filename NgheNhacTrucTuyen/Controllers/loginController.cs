d7dc67ffa4942600e666b2128f8c9d38c5432087
Present111 <161811020+Present111@users.noreply.github.com>
2024-10-23 16:52:09 +0700

    add login form

---
+++ NgheNhacTrucTuyen/Controllers/loginController.cs
@@ -0,0 +1,36 @@
+﻿using NgheNhacTrucTuyen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace NgheNhacTrucTuyen.Controllers
+{
+    public class loginController : Controller
+    {
+        // GET: login
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string email, string password)
+        {
+            DBcontextDataContext context = new DBcontextDataContext();
+            bool data = context.accounts.Any(x => x.Email == email && x.PassWord == password);
+            account a = context.accounts.FirstOrDefault(x => x.Email == email && x.PassWord == password);
+            if (data)
+            {
+                FormsAuthentication.SetAuthCookie(a.Ten, false);
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.error = "Tên đăng nhập hoặc mật khẩu không đúng";
+            return View();
+        }
+    }
+}
